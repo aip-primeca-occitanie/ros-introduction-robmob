a7e5ef3e9a316c174b9e92045531cabdc76a2fd1
Olivier Stasse <ostasse@laas.fr>
2020-11-03 23:02:15 +0100

    Remove X*lock X11-unix files

--- ros-vnc/entrypoint.sh
+++ ros-vnc/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf /tmp/.X*lock
+rm -rf /tmp/.X11-unix
+
 tigervncserver -SecurityTypes None
 sed -i 's/$(hostname)/localhost/g' /usr/share/novnc/utils/launch.sh
 /usr/share/novnc/utils/launch.sh --vnc localhost:5901
